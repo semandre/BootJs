ee4cfb6a23ade206591feac7b751197b8a3d7b3b
RkVishko <rskvishko@gmail.com>
2018-01-16 22:47:50 +0200

    disabled submit with jquery

--- src/main/resources/static/test.js
+++ src/main/resources/static/test.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    console.log("AAAr")
+    $("#saveUser").prop("disabled",true);
+});
+
 function checkFirst() {
     $("#first_name").empty();
     p_sender = document.myform.firstName.value.toString();
@@ -5,10 +10,12 @@ function checkFirst() {
         if (p_sender.length < 3 || p_sender.length > 20) {
             let pp = $('<p>Шмя має складатись віз 3 до 20 літер </p>');
             $("#first_name").append(pp);
+            return true;
         }
     } else {
         let pp = $('<p>Внесіть ваше імя</p>');
         $("#first_name").append(pp);
+        return false;
     }
 }
 
@@ -19,31 +26,37 @@ function checkLast() {
         if (p_sender.length < 3 || p_sender.length > 20) {
             let pp = $('<p>Шмя має складатись віз 3 до 20 літер </p>');
             $("#last_name").append(pp);
+            return true;
         }
     } else {
         let pp = $('<p>Внесіть ваше імя</p>');
         $("#last_name").append(pp);
+        return false;
     }
 }
 
 
 function phonecheck() {
     $("#phone_number").empty();
-    var re =  /^[0-9\-\+]{12}$/;
+    var re = /^[0-9\-\+]{12}$/;
     p_phone = document.myform.phoneNumber.value.toString();
     var valid = re.exec(p_phone);
     if (valid) {
         let p = $('<p>Формат номеру  є корректним</p>');
         $("#phone_number").append(p);
+        $("#saveUser").prop("disabled",false);
+        return true;
     }
-        else {
+    else {
         let p = $('<p>Неправильний формат номеру </p>');
         $("#phone_number").append(p);
+        $("#saveUser").prop("disabled",true);
+        return false;
     }
     console.log(valid);
     console.log(p_phone);
 
-    return valid;
+
 }
 
 
@@ -53,13 +66,17 @@ function checkEmail() {
     var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,3}$/i;
     var valid = re.exec(p_sender);
     if (valid) {
-            let pp = $('<p>Email є корректним</p>');
-            $("#email_").append(pp);
+        let pp = $('<p>Email є корректним</p>');
+        $("#email_").append(pp);
+        return true;
 
     } else {
         let pp = $('<p>Невірний формат email</p>');
         $("#email_").append(pp);
+        return false;
 
     }
-    return valid;
+
+
 }
+
