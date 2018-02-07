9bb9f1df31bf08a4392a3e3f083e8158861e4958
RkVishko <rskvishko@gmail.com>
2018-02-07 15:59:39 +0200

    fixed bugs with modal cart

--- src/main/resources/static/cartmodal.js
+++ src/main/resources/static/cartmodal.js
@@ -1,67 +1,73 @@
 angular.module("modal", [])
-    .controller('modalcarts', function($scope){
+    .controller('modalcarts', function($scope,$timeout){
         $scope.cartbox_show = true;
         $scope.open = function () {
             $scope.cartbox_show = false;
             $scope.cartsArray = JSON.parse(localStorage.getItem("carts"));
+
+            console.log($scope.cartsArray);
+            $timeout(function () {
+                $scope.cartsArray = JSON.parse(localStorage.getItem("carts"));
+                $scope.$apply();
+            });
         };
         $scope.close = function () {
             $scope.cartbox_show = true;
         };
 
-    })
 
+        if($scope.cartsArray == null){
+            $scope.cartsArray = [];
+        }
 
+        $scope.but_show = false;
 
-.controller("carts", function ($scope, $http) {
-    $scope.cartsArray = JSON.parse(localStorage.getItem("carts"));
-    $scope.but_show = false;
-
-    if ($scope.cartsArray.length == 0) {
-        $scope.but_show = true;
-    }
-
-
-    $scope.removeOne = function ($index) {
-        console.log("delete" + $index);
-        $scope.cartsArray.splice($index, 1);
-
-        localStorage.setItem("carts", JSON.stringify($scope.cartsArray));
         if ($scope.cartsArray.length == 0) {
             $scope.but_show = true;
         }
 
 
-    };
-    $scope.increment = function ($index) {
-        console.log("increment" + $index);
-        var a = $scope.cartsArray[$index].quantity;
-        $scope.cartsArray[$index].quantity = a + 1;
-        localStorage.setItem("carts", JSON.stringify($scope.cartsArray));
-    };
-    $scope.decrement = function ($index) {
-        console.log("decrement" + $index);
-        var a = $scope.cartsArray[$index].quantity;
-        if (a > 1) {
-            $scope.cartsArray[$index].quantity = a - 1;
+        $scope.removeOne = function ($index) {
+            console.log("delete" + $index);
+            $scope.cartsArray.splice($index, 1);
+
             localStorage.setItem("carts", JSON.stringify($scope.cartsArray));
-        }
+            if ($scope.cartsArray.length == 0) {
+                $scope.but_show = true;
+            }
 
-    };
+
+        };
+        $scope.increment = function ($index) {
+            console.log("increment" + $index);
+            var a = $scope.cartsArray[$index].quantity;
+            $scope.cartsArray[$index].quantity = a + 1;
+            localStorage.setItem("carts", JSON.stringify($scope.cartsArray));
+        };
+        $scope.decrement = function ($index) {
+            console.log("decrement" + $index);
+            var a = $scope.cartsArray[$index].quantity;
+            if (a > 1) {
+                $scope.cartsArray[$index].quantity = a - 1;
+                localStorage.setItem("carts", JSON.stringify($scope.cartsArray));
+            }
+
+        };
 
 
-    $scope.buy = function () {
+        $scope.buy = function () {
 
-        // var config = {
-        //     headers: {'Content-Type': 'application/json'}
-        // };
-        //
-        // $http.post('addCarts', $scope.cartsArray, config).then(function () {
-        //     console.log("succses");
-        // });
+            // var config = {
+            //     headers: {'Content-Type': 'application/json'}
+            // };
+            //
+            // $http.post('addCarts', $scope.cartsArray, config).then(function () {
+            //     console.log("succses");
+            // });
 
-        console.log("buy log");
+            console.log("buy log");
 
-    };
+        };
 
-});
+    });
+
